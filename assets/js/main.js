7d08ab4df5fed82df6e1d69a43394071749ea723
Khin Nyein Chan <sshan02001@gmail.com>
2023-06-29 23:43:12 +0100

    add moving img, test,getTicketbtn etc

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,36 +64,7 @@
   /**
    * Scrolls to an element with header offset
    */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
-
-    if (!header.classList.contains('header-scrolled')) {
-      offset -= 20
-    }
-
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
-
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
-  let selectHeader = select('#header')
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
-      } else {
-        selectHeader.classList.remove('header-scrolled')
-      }
-    }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
-  }
+  
 
   /**
    * Back to top button
@@ -101,11 +72,9 @@
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
+      
         backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
+      
     }
     window.addEventListener('load', toggleBacktotop)
     onscroll(document, toggleBacktotop)
