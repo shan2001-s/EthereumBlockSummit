72d6b83f40ad12408d65972c500c28bf78594a1d
Khin Nyein Chan <sshan02001@gmail.com>
2023-07-01 20:37:39 +0100

    footer and speakers change

--- assets/js/showSpeakerDetail.js
+++ assets/js/showSpeakerDetail.js
@@ -151,7 +151,6 @@ var speakers = [
     // Add more speaker objects as needed
 ];
 
-
 function createSpeakerDiv(speaker) {
     var speakerDiv = document.createElement("div");
     speakerDiv.classList.add("col-lg-3", "col-md-6");
@@ -164,21 +163,35 @@ function createSpeakerDiv(speaker) {
     var imageDiv = document.createElement("div");
     imageDiv.classList.add("image-container");
   
+    var twitterLogoDiv = document.createElement("div");
+    twitterLogoDiv.classList.add("twitter-logo-container");
+    twitterLogoDiv.style.position = "absolute";
+    twitterLogoDiv.style.top = "0";
+    twitterLogoDiv.style.right = "0";
+    twitterLogoDiv.style.margin = "10px";
+  
+    var twitterLogo = document.createElement("img");
+    twitterLogo.setAttribute("src", "assets/img/twitter-logo.png");
+    twitterLogo.setAttribute("alt", "Twitter Logo");
+    twitterLogo.style.height = "30px";
+  
+    twitterLogoDiv.appendChild(twitterLogo);
+    imageDiv.appendChild(twitterLogoDiv);
+  
     var image = document.createElement("img");
     image.setAttribute("src", speaker.image);
     image.setAttribute("alt", speaker.name);
     image.classList.add("img-fluid");
     image.style.cursor = "pointer";
     image.style.zIndex = -1;
-    image.style.height='300px'
-    image.style.objectFit='cover'
+    image.style.height = '300px';
+    image.style.objectFit = 'cover';
     image.setAttribute("data-bs-toggle", "modal");
     image.setAttribute("data-bs-target", "#speakerModal");
   
     image.addEventListener("click", function () {
       showSpeakerDetails(speaker);
     });
-    
   
     var logoDiv = document.createElement("div");
     logoDiv.classList.add("logo-container");
@@ -188,10 +201,10 @@ function createSpeakerDiv(speaker) {
     speakerLogo.setAttribute("alt", "Speaker Logo");
     speakerLogo.classList.add("speaker-logo");
     speakerLogo.style.height = "50px";
-    speakerLogo.style.position='absolute'
-    speakerLogo.style.zIndex=1
+    speakerLogo.style.position = 'absolute';
+    speakerLogo.style.zIndex = 1;
     speakerLogo.style.borderRadius = "10%";
-    speakerLogo.style.marginBottom='-20px'
+    speakerLogo.style.marginBottom = '-20px';
   
     var detailsDiv = document.createElement("div");
     detailsDiv.classList.add("details");
@@ -205,90 +218,50 @@ function createSpeakerDiv(speaker) {
     speakerName.style.cursor = "pointer";
     speakerName.setAttribute("data-bs-toggle", "modal");
     speakerName.setAttribute("data-bs-target", "#speakerModal");
-    speakerName.addEventListener("click", function () {
-      showSpeakerDetails(speaker);
-    });
   
-    var speakerDescription = document.createElement("p");
-    speakerDescription.textContent = speaker.description;
-  
-    var seeMoreBtn = document.createElement("p");
-    var speakerLink = document.createElement("a");
-    speakerLink.textContent = speaker.morebtn;
-    seeMoreBtn.appendChild(speakerLink);
-    seeMoreBtn.style.cursor = "pointer";
-    seeMoreBtn.setAttribute("data-bs-toggle", "modal");
-    seeMoreBtn.setAttribute("data-bs-target", "#speakerModal");
-    seeMoreBtn.addEventListener("click", function () {
-      showSpeakerDetails(speaker);
-    });
-  
+    speakerDiv.appendChild(innerDiv);
+    innerDiv.appendChild(imageDiv);
+    innerDiv.appendChild(logoDiv);
+    innerDiv.appendChild(detailsDiv);
     imageDiv.appendChild(image);
     logoDiv.appendChild(speakerLogo);
     detailsDiv.appendChild(speakerName);
-    detailsDiv.appendChild(speakerDescription);
-    detailsDiv.appendChild(seeMoreBtn);
-    
-    
-    innerDiv.appendChild(logoDiv);
-    innerDiv.appendChild(imageDiv);
-    innerDiv.appendChild(detailsDiv);
-  
-    speakerDiv.appendChild(innerDiv);
   
     return speakerDiv;
   }
   
   function showSpeakerDetails(speaker) {
-    var speakerName = document.createElement("h3");
-    speakerName.textContent = speaker.name;
+    var speakerModalLabel = document.getElementById("speakerModalLabel");
+    var speakerImage = document.getElementById("speakerImage");
+    var speakerName = document.getElementById("speakerName");
+    var speakerDescription = document.getElementById("speakerDescription");
+    var seeMoreBtn = document.getElementById("seeMoreBtn");
   
-    var speakerImage = document.createElement("img");
+    speakerModalLabel.textContent = speaker.name;
     speakerImage.setAttribute("src", speaker.image);
     speakerImage.setAttribute("alt", speaker.name);
-    speakerImage.classList.add("img-fluid");
-  
-    var speakerDescription = document.createElement("p");
+    speakerName.textContent = speaker.name;
     speakerDescription.textContent = speaker.description;
-    speakerDescription.style.color='black'
-  
-    var speakerDetail = document.createElement("p");
-    speakerDetail.textContent = speaker.detail;
-    speakerDetail.style.color='black'
-  
-    var modalBody = document.getElementById("speakerModalBody");
-    modalBody.innerHTML = "";
-  
-    modalBody.appendChild(speakerImage);
-    modalBody.appendChild(speakerName);
-    modalBody.appendChild(speakerDescription);
-    modalBody.appendChild(speakerDetail);
+    seeMoreBtn.textContent = speaker.morebtn;
+    seeMoreBtn.setAttribute("data-bs-dismiss", "modal");
   }
   
-  window.addEventListener("DOMContentLoaded", function () {
-    // for main page to show 8 speakers
-    var testShow ;
-
-    // for show all speakers
-    var allShow;
-
-    if ( allShow = document.getElementById("allShow")) {
-      for (var j = 0; j < speakers.length; j++) {
-        var speakersDiv = createSpeakerDiv(speakers[j]);
-        allShow.appendChild(speakersDiv);
-      }
-      
-    }
-
-
-    else if (testShow= document.getElementById("testShow")){
-        for (var i = 0; i < 8; i++) {
-            var speakerDiv = createSpeakerDiv(speakers[i]);
-            testShow.appendChild(speakerDiv);
-          }
-      }
-   
+  function loadSpeakers() {
+    var speakersContainer = document.getElementById("speakersContainer");
+    var speakersRow = document.getElementById("speakersRow");
+  
+    speakers.forEach(function (speaker) {
+      var speakerDiv = createSpeakerDiv(speaker);
+      speakersRow.appendChild(speakerDiv);
+    });
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    loadSpeakers();
   });
+  
+  
+
 
 
 
